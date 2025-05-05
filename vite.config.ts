c66820d280346bed0b13897690c23a5c7ad3573b
kkhanh-kas <kkhanhnnuyen@gmail.com>
2025-05-05 21:29:12 +0700

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Aurora/',
+  base: '/chatbot/',
   plugins: [react()],
   server: {
     port: 5173,
